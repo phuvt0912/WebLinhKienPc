0e4a6f3c09b1a2e9890459471fbfcf3e45b58de0
MinhThang <dominhtoan9467@gmail.com>
2026-03-22 08:52:22 +0700

    Setup để làm profile

---
+++ Models/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebLinhKienPc.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string? AvatarUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
