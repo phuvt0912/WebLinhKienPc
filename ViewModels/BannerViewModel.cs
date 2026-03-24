3e1dbe68df5f864a9634c449877c24388c9591ad
akzm <phamsyphu0912@gmail.com>
2026-03-24 11:20:37 +0700

    add banner CRUD UI

---
+++ ViewModels/BannerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebLinhKienPc.Models;
+
+namespace WebLinhKienPc.ViewModels
+{
+	public class BannerViewModel
+	{
+		public List<Banner> Banners { get; set; }
+		public string Keyword { get; set; }
+	}
+}
