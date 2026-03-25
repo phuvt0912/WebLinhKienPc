85f0e9bc8eb84534f32eb86015416859557d1c12
MinhThang <dominhtoan9467@gmail.com>
2026-03-25 08:08:29 +0700

    sửa lại UI cho Home

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,17 +1,24 @@
-﻿using WebLinhKienPc.Models;
+using WebLinhKienPc.Models;
 
 namespace WebLinhKienPc.ViewModels
 {
 	public class HomeViewModel
 	{
-		public List<Product> HotProducts { get; set; }
+		public List<HotProductItem> HotProducts { get; set; }
 		public List<Product> NewProducts { get; set; }
 		public List<Banner> Banners { get; set; }
 		public List<CategoryWithProducts> Categories { get; set; }
 	}
 
+	public class HotProductItem
+	{
+		public Product Product { get; set; }
+		public int SoldCount { get; set; }
+	}
+
 	public class CategoryWithProducts
 	{
+		public int CategoryId { get; set; }
 		public string CategoryName { get; set; }
 		public List<Product> Products { get; set; }
 	}
